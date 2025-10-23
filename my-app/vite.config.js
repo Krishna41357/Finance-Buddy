182c8da886f56319fcf15d74292b54e9daf664fe
Krishna41357 <154420450+Krishna41357@users.noreply.github.com>
2025-10-23 15:15:03 +0530

    main

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,20 +2,21 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
-  // Load env variables based on the mode (development/production)
   const env = loadEnv(mode, process.cwd())
 
   return {
     plugins: [react()],
+    base: './', // ✅ Important for Vercel / relative paths
     build: {
       rollupOptions: {
         external: ["react-chartjs-2", "chart.js"],
-      }
+      },
+      chunkSizeWarningLimit: 2000, // optional, to avoid large chunk warnings
     },
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL, // ✅ Loaded from .env file
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
           secure: false,
         }
